169a54a93f92fced5910bcd07e4123b6f2e8ddda
Michael Chen <chen.michael9@gmail.com>
2017-04-11 01:15:28 -0700

    Data Retrieval from time-series data working

--- historical-telemetry-plugin.js
+++ historical-telemetry-plugin.js
@@ -2,6 +2,8 @@
  * Basic historical telemetry plugin.
  */
 
+var currentYear = 1260;
+
 function HistoricalTelemetryPlugin() {
     return function install (openmct) {
         var provider = {
@@ -9,13 +11,14 @@ function HistoricalTelemetryPlugin() {
                 return domainObject.type === 'worker.telemetry';
             },
             request: function (domainObject, options) {
-                var url = 'http://datamarket.com/api/v1/list.json?ds=' + domainObject.telemetry.key + '!1=[' + options.start + ']';
                 
-                console.log(url + '');
+                var url = 'http://datamarket.com/api/v1/list.json?ds=' + domainObject.telemetry.key + '!1=[' + currentYear + ']';
+                
+                currentYear += 1;
 
                 return http.get(url)
                     .then(function (resp) {
-                        console.log(resp.data+'');
+                        console.log(resp.data);
                         return resp.data;
                     });
             }
